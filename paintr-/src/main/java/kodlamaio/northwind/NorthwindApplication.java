2cbf51ab44becb27e76dfa724831f472e4a62684
mammarkk <mammarkk@hotmail.com>
2023-04-12 14:51:23 +0300

    finish

--- paintr-/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ paintr-/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -12,8 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-
-
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
@@ -24,7 +21,7 @@ public class NorthwindApplication {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))                                        
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))                                     
           .build();                                           
     }
 
